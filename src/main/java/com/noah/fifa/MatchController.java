069500d17d48c3babc91ecf9462035cc9e5c44a9
Emily <contact.eemilyycho@gmail.com>
2024-06-06 08:23:32 -0400

    Jun 6

--- src/main/java/com/noah/fifa/MatchController.java
+++ src/main/java/com/noah/fifa/MatchController.java
@@ -1,16 +1,28 @@
 package com.noah.fifa;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/matches")
 public class MatchController {
+    private String jsonFile= "src/main/resources/static/static.json";
+    private void saveToFile(List<Matches> matches){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try(FileWriter fileWriter = new FileWriter(jsonFile)){
+            objectMapper.writeValue(fileWriter, matches);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     @Autowired
     private MatchService mservice;
 
